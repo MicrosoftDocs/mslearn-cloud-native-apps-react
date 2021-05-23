ce0e945edd344f0840677c182bcdc9234a84eb46
Seth Reid <seth.reid@xero.com>
2021-05-23 13:54:55 +1200

    fixe

--- scripts/copyToOutput.sh
+++ scripts/copyToOutput.sh
@@ -7,5 +7,6 @@ mkdir -p $2
 cp -R $sourceFolder/.next $targetFolder/.next
 cp $sourceFolder/package.json $targetFolder/package.json
 cp $sourceFolder/web.config $targetFolder/web.config
+cp $sourceFolder/package.json $targetFolder/server.js
 cp -R $sourceFolder/public $targetFolder/public
 cp -R $sourceFolder/node_modules $targetFolder/node_modules
