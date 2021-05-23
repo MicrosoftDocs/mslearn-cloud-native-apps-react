9cd6b84170a17c84c212e9023653716954f87597
Seth Reid <seth.reid@xero.com>
2021-05-23 13:29:56 +1200

    copy pakcge.josn

--- scripts/copyToOutput.sh
+++ scripts/copyToOutput.sh
@@ -6,5 +6,6 @@ targetFolder=$2
 mkdir -p $2
 cp -R $sourceFolder/.next $targetFolder/.next
 cp $sourceFolder/package.json $targetFolder/package.json
+cp $sourceFolder/web.config $targetFolder/web.config
 cp -R $sourceFolder/public $targetFolder/public
 cp -R $sourceFolder/node_modules $targetFolder/node_modules
